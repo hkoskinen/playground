5d80701061140bcd1de652acb880622ec0c6885c
Henri Koskinen <hkoskinen@protonmail.com>
2018-07-10 20:54:22 +0300

    Add first controller

---
+++ src/main/java/com/gravenium/playground/controllers/IndexController.java
@@ -0,0 +1,21 @@
+package com.gravenium.playground.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String home() {
+        return "Welcome Home!";
+    }
+
+    @RequestMapping("/about")
+    @ResponseBody
+    public String about() {
+        return "About this thing";
+    }
+}
